f847bae6d5b6d110d49c96fb807de0e87c0db494
Emma Lyddon <elyddon@gmail.com>
2022-01-17 02:02:29 +0000

    Cleanup 1

--- static/js/journal_search.js
+++ static/js/journal_search.js
@@ -1,9 +1,11 @@
 'use strict';
 
+// Select the search-by-date button when clicked
 const dateBtn = document.querySelector('#jrnl-search-submit').addEventListener('click', (evt) => {
     evt.preventDefault();
     console.log('Ive been clicked');
 
+    // Get the user's selected date
     const date = document.querySelector('#jrnl-search-by-date').value;
 
     console.log(date);
@@ -12,22 +14,24 @@ const dateBtn = document.querySelector('#jrnl-search-submit').addEventListener('
         date: date,
     };
 
-
+    // Send a post fetch request to the server-side with the user's input value
     fetch('/journal-date-results.json', {   
         method: 'POST',
         body: JSON.stringify(jrnlDate),
         headers: {
             'Content-Type': 'application/json'
         },
-    // return the promised response in JSON
+    // Return the promised response in JSON and parse it to produce a javascript object
     })
-        .then(response => response.json())
+        .then(response => response.json()) 
         .then(responseData => {
             console.log(responseData);
 
+            // For loop over the object
             for (const key of Object.keys(responseData)) {
                 console.log(key);
 
+                // If the response data is not none, retrieve all the necessary values to display onto the page
                 if (responseData[key] != "none") {
                     const mnth = responseData[key]['mnth'];
                     const timeStamp = responseData[key]['time_stamp'];
@@ -41,6 +45,7 @@ const dateBtn = document.querySelector('#jrnl-search-submit').addEventListener('
                     const gratitude3 = responseData[key]['gratitude_3'];
                     const jrnlInput = responseData[key]['journal_input'];
 
+                    // Select this div container, manipulate the dom to display the results on the page without refreshing
                     document.querySelector('#display-entries-container').insertAdjacentHTML('beforeend', 
                     `<div class="row jrnl-search-results">
                         <div class="col-12 jrnl-search-container entry">
@@ -56,6 +61,7 @@ const dateBtn = document.querySelector('#jrnl-search-submit').addEventListener('
                             </div>
                         </div><br>
                     </div>`)
+                  // If no journal entries exist, insert a statement 
                 } else if (responseData[key] == {}) {
                     document.querySelector('#display-entries-container').insertAdjacentHTML('beforeend', `<p class="jrnl-search-no-entries">No journal entries match your date</p>`)
                 } 
@@ -66,18 +72,21 @@ const dateBtn = document.querySelector('#jrnl-search-submit').addEventListener('
 });
 
 
-
+// Select the view all journal entries button when clicked
 const allJrnlsBtn = document.querySelector('#all-jrnls')
 allJrnlsBtn.addEventListener('click', evt => {
     evt.preventDefault();
     console.log('the view all journal entries button has been clicked');
 
+    // Send a fetch request to the server side
     fetch('/journal-data.json')
         .then(response => response.json())
         .then(responseData => {
 
+            // For loop over the parsed data
             for (const key of Object.keys(responseData)) {
 
+                // If a journal entry exists, retrieve all the necessary values to display onto the page
                 if (responseData[key] != "no journal entries exist") {
                     const mnth = responseData[key]['mnth'];
                     const timeStamp = responseData[key]['time_stamp'];
@@ -91,6 +100,7 @@ allJrnlsBtn.addEventListener('click', evt => {
                     const gratitude3 = responseData[key]['gratitude_3'];
                     const jrnlInput = responseData[key]['journal_input'];
 
+                    // Select this div container, manipulate the dom to display the results on the page without refreshing
                     document.querySelector('#display-entries-container').insertAdjacentHTML('beforeend', 
                     `<div class="row jrnl-search-results">
                         <div class="col-12 jrnl-search-container entry">
@@ -106,6 +116,10 @@ allJrnlsBtn.addEventListener('click', evt => {
                             </div>
                         </div><br>
                     </div>`)
+                // If no journal entries exist, insert a statement 
+                } else if (responseData[key] == {}) {
+                    document.querySelector('#display-entries-container').insertAdjacentHTML('beforeend', `<p class="jrnl-search-no-entries">No journal entries exist yet</p>`)
+        
                 }
             }
         });
